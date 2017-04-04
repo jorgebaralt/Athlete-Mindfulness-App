16aee9fef5b12fbf6d21b06805bc4ac6026a537d
Jorge Baralt <jorgebaraltq@gmail.com>
2017-04-04 13:14:45 -0400

    Fixed navigation drawer, now when logged in, goes directly to profile fragment

--- app/src/main/java/com/jorgebaralt/athlete_mindful_app/NavigationDrawer.java
+++ app/src/main/java/com/jorgebaralt/athlete_mindful_app/NavigationDrawer.java
@@ -1,11 +1,9 @@
 package com.jorgebaralt.athlete_mindful_app;
 
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -25,14 +23,6 @@ public class NavigationDrawer extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,6 +32,9 @@ public class NavigationDrawer extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //when they are logged in, go directly to their profile fragment.
+        displaySelectedScreen(R.id.nav_profile);
 
 
     }
@@ -82,27 +75,38 @@ public class NavigationDrawer extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+
+        //Select the ID clicked from Nav-Draw.
         int id = item.getItemId();
-        FragmentManager fragmentManager = getFragmentManager();
 
-        if (id == R.id.nav_survey) {
-             goToSurvey();
-        } else if (id == R.id.nav_profile) {
-            fragmentManager.beginTransaction().replace(R.id.content_frame,new Profile()).commit();
-        } else if (id == R.id.nav_leaderboard) {
-            fragmentManager.beginTransaction().replace(R.id.content_frame,new Leaderboard()).commit();
-        } else if (id == R.id.nav_manage) {
+        //Function that changes between fragments
+        displaySelectedScreen(id);
 
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        return true;
+    }
+    private void displaySelectedScreen(int id){
+        Fragment fragment = null;
+        switch(id){
+            case R.id.nav_survey:
+                goToSurvey();
+                break;
+            case R.id.nav_profile:
+                fragment = new Profile();
+                break;
+            case R.id.nav_leaderboard:
+                fragment = new Leaderboard();
+                break;
+            case R.id.nav_chat:
+                //TODO: start chat, refer to selected API
+                fragment = new Fragment();
         }
-
+        if(fragment!=null){
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, fragment);
+            fragmentTransaction.commit();
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
     public void goToSurvey(){
         Intent intent = new Intent(this,SurveyActivity.class);
